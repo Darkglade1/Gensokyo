5181bd7ccc0c16ca1b135c6c313a512a2e656025
Darkglade <30379754+Darkglade@users.noreply.github.com>
2019-09-29 15:42:17 -0400

    Restructure

---
+++ src/main/java/Gensokyo/GensokyoMod.java
@@ -0,0 +1,336 @@
+package Gensokyo;
+
+import Gensokyo.events.IdentityCrisisEvent;
+import Gensokyo.events.ScarletDevilMansionEvent;
+import Gensokyo.relics.BottledPlaceholderRelic;
+import Gensokyo.relics.DefaultClickableRelic;
+import Gensokyo.relics.PlaceholderRelic;
+import Gensokyo.relics.PlaceholderRelic2;
+import Gensokyo.util.IDCheckDontTouchPls;
+import Gensokyo.util.TextureLoader;
+import Gensokyo.variables.DefaultCustomVariable;
+import Gensokyo.variables.DefaultSecondMagicNumber;
+import basemod.BaseMod;
+import basemod.ModPanel;
+import basemod.helpers.RelicType;
+import basemod.interfaces.EditCardsSubscriber;
+import basemod.interfaces.EditKeywordsSubscriber;
+import basemod.interfaces.EditRelicsSubscriber;
+import basemod.interfaces.EditStringsSubscriber;
+import basemod.interfaces.PostInitializeSubscriber;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.evacipated.cardcrawl.mod.stslib.Keyword;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.google.gson.Gson;
+import com.megacrit.cardcrawl.dungeons.Exordium;
+import com.megacrit.cardcrawl.dungeons.TheCity;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.localization.EventStrings;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+import com.megacrit.cardcrawl.unlock.UnlockTracker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+//TODO: DON'T MASS RENAME/REFACTOR
+//TODO: DON'T MASS RENAME/REFACTOR
+//TODO: DON'T MASS RENAME/REFACTOR
+//TODO: DON'T MASS RENAME/REFACTOR
+// Please don't just mass replace "Gensokyo" with "yourMod" everywhere.
+// It'll be a bigger pain for you. You only need to replace it in 3 places.
+// I comment those places below, under the place where you set your ID.
+
+//TODO: FIRST THINGS FIRST: RENAME YOUR PACKAGE AND ID NAMES FIRST-THING!!!
+// Right click the package (Open the project pane on the left. Folder with black dot on it. The name's at the very top) -> Refactor -> Rename, and name it whatever you wanna call your mod.
+// Scroll down in this file. Change the ID from "Gensokyo:" to "yourModName:" or whatever your heart desires (don't use spaces). Dw, you'll see it.
+// In the JSON strings (resources>localization>eng>[all them files] make sure they all go "yourModName:" rather than "Gensokyo". You can ctrl+R to replace in 1 file, or ctrl+shift+r to mass replace in specific files/directories (Be careful.).
+// Start with the DefaultCommon cards - they are the most commented cards since I don't feel it's necessary to put identical comments on every card.
+// After you sorta get the hang of how to make cards, check out the card template which will make your life easier
+
+/*
+ * With that out of the way:
+ * Welcome to this super over-commented Slay the Spire modding base.
+ * Use it to make your own mod of any type. - If you want to add any standard in-game content (character,
+ * cards, relics), this is a good starting point.
+ * It features 1 character with a minimal set of things: 1 card of each type, 1 debuff, couple of relics, etc.
+ * If you're new to modding, you basically *need* the BaseMod wiki for whatever you wish to add
+ * https://github.com/daviscook477/BaseMod/wiki - work your way through with this base.
+ * Feel free to use this in any way you like, of course. MIT licence applies. Happy modding!
+ *
+ * And pls. Read the comments.
+ */
+
+@SpireInitializer
+public class GensokyoMod implements
+        EditCardsSubscriber,
+        EditRelicsSubscriber,
+        EditStringsSubscriber,
+        EditKeywordsSubscriber,
+        PostInitializeSubscriber {
+    // Make sure to implement the subscribers *you* are using (read basemod wiki). Editing cards? EditCardsSubscriber.
+    // Making relics? EditRelicsSubscriber. etc., etc., for a full list and how to make your own, visit the basemod wiki.
+    public static final Logger logger = LogManager.getLogger(GensokyoMod.class.getName());
+    private static String modID;
+
+    //This is for the in-game mod settings panel.
+    private static final String MODNAME = "Gensokyo";
+    private static final String AUTHOR = "Darkglade"; // And pretty soon - You!
+    private static final String DESCRIPTION = "A content mod inspired by Touhou Project.";
+    
+    // =============== INPUT TEXTURE LOCATION =================
+    
+    //Mod Badge - A small icon that appears in the mod settings menu next to your mod.
+    public static final String BADGE_IMAGE = "GensokyoResources/images/Badge.png";
+    
+    // =============== MAKE IMAGE PATHS =================
+    
+    public static String makeCardPath(String resourcePath) {
+        return getModID() + "Resources/images/cards/" + resourcePath;
+    }
+    
+    public static String makeRelicPath(String resourcePath) {
+        return getModID() + "Resources/images/relics/" + resourcePath;
+    }
+    
+    public static String makeRelicOutlinePath(String resourcePath) {
+        return getModID() + "Resources/images/relics/outline/" + resourcePath;
+    }
+    
+    public static String makePowerPath(String resourcePath) {
+        return getModID() + "Resources/images/powers/" + resourcePath;
+    }
+    
+    public static String makeEventPath(String resourcePath) {
+        return getModID() + "Resources/images/events/" + resourcePath;
+    }
+    
+    // =============== /MAKE IMAGE PATHS/ =================
+    
+    // =============== /INPUT TEXTURE LOCATION/ =================
+    
+    
+    // =============== SUBSCRIBE, CREATE THE COLOR_GRAY, INITIALIZE =================
+    
+    public GensokyoMod() {
+        logger.info("Subscribe to BaseMod hooks");
+        
+        BaseMod.subscribe(this);
+        
+      /*
+           (   ( /(  (     ( /( (            (  `   ( /( )\ )    )\ ))\ )
+           )\  )\()) )\    )\()))\ )   (     )\))(  )\()|()/(   (()/(()/(
+         (((_)((_)((((_)( ((_)\(()/(   )\   ((_)()\((_)\ /(_))   /(_))(_))
+         )\___ _((_)\ _ )\ _((_)/(_))_((_)  (_()((_) ((_|_))_  _(_))(_))_
+        ((/ __| || (_)_\(_) \| |/ __| __| |  \/  |/ _ \|   \  |_ _||   (_)
+         | (__| __ |/ _ \ | .` | (_ | _|  | |\/| | (_) | |) |  | | | |) |
+          \___|_||_/_/ \_\|_|\_|\___|___| |_|  |_|\___/|___/  |___||___(_)
+      */
+      
+        setModID("Gensokyo");
+        // cool
+        // TODO: NOW READ THIS!!!!!!!!!!!!!!!:
+        
+        // 1. Go to your resources folder in the project panel, and refactor> rename GensokyoResources to
+        // yourModIDResources.
+        
+        // 2. Click on the localization > eng folder and press ctrl+shift+r, then select "Directory" (rather than in Project)
+        // replace all instances of Gensokyo with yourModID.
+        // Because your mod ID isn't the default. Your cards (and everything else) should have Your mod id. Not mine.
+        
+        // 3. FINALLY and most importantly: Scroll up a bit. You may have noticed the image locations above don't use getModID()
+        // Change their locations to reflect your actual ID rather than Gensokyo. They get loaded before getID is a thing.
+        
+        logger.info("Done subscribing");
+        
+    }
+    
+    // ====== NO EDIT AREA ======
+    // DON'T TOUCH THIS STUFF. IT IS HERE FOR STANDARDIZATION BETWEEN MODS AND TO ENSURE GOOD CODE PRACTICES.
+    // IF YOU MODIFY THIS I WILL HUNT YOU DOWN AND DOWNVOTE YOUR MOD ON WORKSHOP
+    
+    public static void setModID(String ID) { // DON'T EDIT
+        Gson coolG = new Gson(); // EY DON'T EDIT THIS
+        //   String IDjson = Gdx.files.internal("IDCheckStringsDONT-EDIT-AT-ALL.json").readString(String.valueOf(StandardCharsets.UTF_8)); // i hate u Gdx.files
+        InputStream in = GensokyoMod.class.getResourceAsStream("/IDCheckStringsDONT-EDIT-AT-ALL.json"); // DON'T EDIT THIS ETHER
+        IDCheckDontTouchPls EXCEPTION_STRINGS = coolG.fromJson(new InputStreamReader(in, StandardCharsets.UTF_8), IDCheckDontTouchPls.class); // OR THIS, DON'T EDIT IT
+        logger.info("You are attempting to set your mod ID as: " + ID); // NO WHY
+        if (ID.equals(EXCEPTION_STRINGS.DEFAULTID)) { // DO *NOT* CHANGE THIS ESPECIALLY, TO EDIT YOUR MOD ID, SCROLL UP JUST A LITTLE, IT'S JUST ABOVE
+            throw new RuntimeException(EXCEPTION_STRINGS.EXCEPTION); // THIS ALSO DON'T EDIT
+        } else if (ID.equals(EXCEPTION_STRINGS.DEVID)) { // NO
+            modID = EXCEPTION_STRINGS.DEFAULTID; // DON'T
+        } else { // NO EDIT AREA
+            modID = ID; // DON'T WRITE OR CHANGE THINGS HERE NOT EVEN A LITTLE
+        } // NO
+        logger.info("Success! ID is " + modID); // WHY WOULD U WANT IT NOT TO LOG?? DON'T EDIT THIS.
+    } // NO
+    
+    public static String getModID() { // NO
+        return modID; // DOUBLE NO
+    } // NU-UH
+    
+    private static void pathCheck() { // ALSO NO
+        Gson coolG = new Gson(); // NNOPE DON'T EDIT THIS
+        //   String IDjson = Gdx.files.internal("IDCheckStringsDONT-EDIT-AT-ALL.json").readString(String.valueOf(StandardCharsets.UTF_8)); // i still hate u btw Gdx.files
+        InputStream in = GensokyoMod.class.getResourceAsStream("/IDCheckStringsDONT-EDIT-AT-ALL.json"); // DON'T EDIT THISSSSS
+        IDCheckDontTouchPls EXCEPTION_STRINGS = coolG.fromJson(new InputStreamReader(in, StandardCharsets.UTF_8), IDCheckDontTouchPls.class); // NAH, NO EDIT
+        String packageName = GensokyoMod.class.getPackage().getName(); // STILL NO EDIT ZONE
+        FileHandle resourcePathExists = Gdx.files.internal(getModID() + "Resources"); // PLEASE DON'T EDIT THINGS HERE, THANKS
+        if (!modID.equals(EXCEPTION_STRINGS.DEVID)) { // LEAVE THIS EDIT-LESS
+            if (!packageName.equals(getModID())) { // NOT HERE ETHER
+                throw new RuntimeException(EXCEPTION_STRINGS.PACKAGE_EXCEPTION + getModID()); // THIS IS A NO-NO
+            } // WHY WOULD U EDIT THIS
+            if (!resourcePathExists.exists()) { // DON'T CHANGE THIS
+                throw new RuntimeException(EXCEPTION_STRINGS.RESOURCE_FOLDER_EXCEPTION + getModID() + "Resources"); // NOT THIS
+            }// NO
+        }// NO
+    }// NO
+    
+    // ====== YOU CAN EDIT AGAIN ======
+    
+    
+    @SuppressWarnings("unused")
+    public static void initialize() {
+        GensokyoMod gensokyoMod = new GensokyoMod();
+    }
+    
+    
+    // =============== POST-INITIALIZE =================
+    
+    @Override
+    public void receivePostInitialize() {
+        logger.info("Loading badge image and mod options");
+        
+        // Load the Mod Badge
+        Texture badgeTexture = TextureLoader.getTexture(BADGE_IMAGE);
+        
+        // Create the Mod Menu
+        ModPanel settingsPanel = new ModPanel();
+        
+        BaseMod.registerModBadge(badgeTexture, MODNAME, AUTHOR, DESCRIPTION, settingsPanel);
+
+        
+        // =============== EVENTS =================
+        
+        // This event will be exclusive to the City (act 2). If you want an event that's present at any
+        // part of the game, simply don't include the dungeon ID
+        // If you want to have a character-specific event, look at slimebound (CityRemoveEventPatch).
+        // Essentially, you need to patch the game and say "if a player is not playing my character class, remove the event from the pool"
+        //BaseMod.addEvent(IdentityCrisisEvent.ID, IdentityCrisisEvent.class, TheCity.ID);
+        BaseMod.addEvent(ScarletDevilMansionEvent.ID, ScarletDevilMansionEvent.class, Exordium.ID);
+        
+        // =============== /EVENTS/ =================
+        logger.info("Done loading badge Image and mod options");
+    }
+    
+    // =============== / POST-INITIALIZE/ =================
+    
+    
+    // ================ ADD RELICS ===================
+    
+    @Override
+    public void receiveEditRelics() {
+        logger.info("Adding relics");
+        
+        // This adds a character specific relic. Only when you play with the mentioned color, will you get this relic.
+        BaseMod.addRelic(new PlaceholderRelic(), RelicType.SHARED);
+        BaseMod.addRelic(new BottledPlaceholderRelic(), RelicType.SHARED);
+        BaseMod.addRelic(new DefaultClickableRelic(), RelicType.SHARED);
+        
+        // This adds a relic to the Shared pool. Every character can find this relic.
+        BaseMod.addRelic(new PlaceholderRelic2(), RelicType.SHARED);
+        
+        // Mark relics as seen (the others are all starters so they're marked as seen in the character file
+        UnlockTracker.markRelicAsSeen(BottledPlaceholderRelic.ID);
+        logger.info("Done adding relics!");
+    }
+    
+    // ================ /ADD RELICS/ ===================
+    
+    
+    // ================ ADD CARDS ===================
+    
+    @Override
+    public void receiveEditCards() {
+        logger.info("Adding variables");
+        //Ignore this
+        pathCheck();
+        // Add the Custom Dynamic Variables
+        logger.info("Add variabls");
+        // Add the Custom Dynamic variabls
+        BaseMod.addDynamicVariable(new DefaultCustomVariable());
+        BaseMod.addDynamicVariable(new DefaultSecondMagicNumber());
+    }
+    
+    // There are better ways to do this than listing every single individual card, but I do not want to complicate things
+    // in a "tutorial" mod. This will do and it's completely ok to use. If you ever want to clean up and
+    // shorten all the imports, go look take a look at other mods, such as Hubris.
+    
+    // ================ /ADD CARDS/ ===================
+    
+    
+    // ================ LOAD THE TEXT ===================
+    
+    @Override
+    public void receiveEditStrings() {
+        logger.info("You seeing this?");
+        logger.info("Beginning to edit strings for mod with ID: " + getModID());
+        
+        // CardStrings
+        BaseMod.loadCustomStringsFile(CardStrings.class,
+                getModID() + "Resources/localization/eng/GensokyoMod-Card-Strings.json");
+        
+        // PowerStrings
+        BaseMod.loadCustomStringsFile(PowerStrings.class,
+                getModID() + "Resources/localization/eng/GensokyoMod-Power-Strings.json");
+        
+        // RelicStrings
+        BaseMod.loadCustomStringsFile(RelicStrings.class,
+                getModID() + "Resources/localization/eng/GensokyoMod-Relic-Strings.json");
+        
+        // Event Strings
+        BaseMod.loadCustomStringsFile(EventStrings.class,
+                getModID() + "Resources/localization/eng/GensokyoMod-Event-Strings.json");
+        
+        logger.info("Done edittting strings");
+    }
+    
+    // ================ /LOAD THE TEXT/ ===================
+    
+    // ================ LOAD THE KEYWORDS ===================
+    
+    @Override
+    public void receiveEditKeywords() {
+        // Keywords on cards are supposed to be Capitalized, while in Keyword-String.json they're lowercase
+        //
+        // Multiword keywords on cards are done With_Underscores
+        //
+        // If you're using multiword keywords, the first element in your NAMES array in your keywords-strings.json has to be the same as the PROPER_NAME.
+        // That is, in Card-Strings.json you would have #yA_Long_Keyword (#y highlights the keyword in yellow).
+        // In Keyword-Strings.json you would have PROPER_NAME as A Long Keyword and the first element in NAMES be a long keyword, and the second element be a_long_keyword
+        
+        Gson gson = new Gson();
+        String json = Gdx.files.internal(getModID() + "Resources/localization/eng/GensokyoMod-Keyword-Strings.json").readString(String.valueOf(StandardCharsets.UTF_8));
+        com.evacipated.cardcrawl.mod.stslib.Keyword[] keywords = gson.fromJson(json, com.evacipated.cardcrawl.mod.stslib.Keyword[].class);
+        
+        if (keywords != null) {
+            for (Keyword keyword : keywords) {
+                BaseMod.addKeyword(getModID().toLowerCase(), keyword.PROPER_NAME, keyword.NAMES, keyword.DESCRIPTION);
+                //  getModID().toLowerCase() makes your keyword mod specific (it won't show up in other cards that use that word)
+            }
+        }
+    }
+    
+    // ================ /LOAD THE KEYWORDS/ ===================    
+    
+    // this adds "ModName:" before the ID of any card/relic/power etc.
+    // in order to avoid conflicts if any other mod uses the same ID.
+    public static String makeID(String idText) {
+        return getModID() + ":" + idText;
+    }
+}
