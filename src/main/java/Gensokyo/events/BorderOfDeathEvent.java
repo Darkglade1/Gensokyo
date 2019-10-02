72b8991039bc0b02d1983c0a31645e7f45db5839
Darkglade <30379754+Darkglade@users.noreply.github.com>
2019-10-01 22:11:05 -0400

    Added Border of Death event in full

--- src/main/java/Gensokyo/events/BorderOfDeathEvent.java
+++ src/main/java/Gensokyo/events/BorderOfDeathEvent.java
@@ -1,20 +1,14 @@
 package Gensokyo.events;
 
 import Gensokyo.GensokyoMod;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.CardGroup;
+import Gensokyo.relics.PerfectCherryBlossom;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.events.AbstractImageEvent;
 import com.megacrit.cardcrawl.helpers.ScreenShake;
 import com.megacrit.cardcrawl.localization.EventStrings;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-import com.megacrit.cardcrawl.unlock.UnlockTracker;
-import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
-
-import java.util.Iterator;
 
 import static Gensokyo.GensokyoMod.makeEventPath;
 
@@ -30,22 +24,9 @@ public class BorderOfDeathEvent extends AbstractImageEvent {
     public static final String IMG = makeEventPath("BorderOfDeath.png");
 
     private int screenNum = 0; // The initial screen we will see when encountering the event - screen 0;
-    private boolean pickCard = false;
-
-    private float HEALTH_LOSS_PERCENTAGE = 0.20F; // 20%
-    private float HEALTH_LOSS_PERCENTAGE_HIGH_ASCENSION = 0.25F; // 25%
-
-    private int healthdamage;
 
     public BorderOfDeathEvent() {
         super(NAME, DESCRIPTIONS[0], IMG);
-
-        if (AbstractDungeon.ascensionLevel < 15) {
-            healthdamage = (int) ((float) AbstractDungeon.player.maxHealth * HEALTH_LOSS_PERCENTAGE);
-        } else {
-            healthdamage = (int) ((float) AbstractDungeon.player.maxHealth * HEALTH_LOSS_PERCENTAGE_HIGH_ASCENSION);
-        }
-
         // The first dialogue options available to us.
         imageEventText.setDialogOption(OPTIONS[0]);
 
@@ -60,101 +41,62 @@ public class BorderOfDeathEvent extends AbstractImageEvent {
                 break;
             case 1:
                 this.imageEventText.updateBodyText(DESCRIPTIONS[2]);
-                this.imageEventText.clearAllDialogs();
-                this.imageEventText.setDialogOption(OPTIONS[2]); // Make donation
-                this.imageEventText.setDialogOption(OPTIONS[1]); //Go to library
+                this.imageEventText.updateDialogOption(0, OPTIONS[3]);
                 screenNum = 2;
                 break;
             case 2:
+                this.imageEventText.updateBodyText(DESCRIPTIONS[3]);
+                this.imageEventText.updateDialogOption(0, OPTIONS[0]);
+                screenNum = 3;
+                break;
+            case 3:
+                this.imageEventText.updateBodyText(DESCRIPTIONS[4]);
+                this.imageEventText.clearAllDialogs();
+                this.imageEventText.setDialogOption(OPTIONS[2]); // Die
+                this.imageEventText.setDialogOption(OPTIONS[1]); //Refuse
+                screenNum = 4;
+                break;
+            case 4:
                 switch (buttonPressed) {
-                    case 0: //Go to library
-                        this.imageEventText.updateBodyText(DESCRIPTIONS[3]);
-                        screenNum = 3;
-                        this.imageEventText.updateDialogOption(0, OPTIONS[4]);
-                        this.imageEventText.clearRemainingOptions();
-                        this.pickCard = true;
-                        CardGroup group = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
-
-                        for(int i = 0; i < 10; ++i) {
-                            AbstractCard card = AbstractDungeon.getCard(AbstractDungeon.rollRarity()).makeCopy();
-                            boolean containsDupe = true;
-
-                            while(true) {
-                                while(containsDupe) {
-                                    containsDupe = false;
-                                    Iterator var6 = group.group.iterator();
-
-                                    while(var6.hasNext()) {
-                                        AbstractCard c = (AbstractCard)var6.next();
-                                        if (c.cardID.equals(card.cardID)) {
-                                            containsDupe = true;
-                                            card = AbstractDungeon.getCard(AbstractDungeon.rollRarity()).makeCopy();
-                                            break;
-                                        }
-                                    }
-                                }
-
-                                if (group.contains(card)) {
-                                    --i;
-                                } else {
-                                    card.upgrade();
-                                    group.addToBottom(card);
-                                }
-                                break;
-                            }
-                        }
-
-                        Iterator var8 = group.group.iterator();
-
-                        while(var8.hasNext()) {
-                            AbstractCard c = (AbstractCard)var8.next();
-                            UnlockTracker.markCardAsSeen(c.cardID);
-                        }
-
-                        AbstractDungeon.gridSelectScreen.open(group, 1, OPTIONS[5], false);
-
-                        break;
-                    case 1: // Make donation
-                        this.imageEventText.updateBodyText(DESCRIPTIONS[4]);
-                        screenNum = 4;
+                    case 0: // Die
+                        this.imageEventText.updateBodyText(DESCRIPTIONS[5]);
+                        screenNum = 5;
                         this.imageEventText.updateDialogOption(0, OPTIONS[0]);
                         this.imageEventText.clearRemainingOptions();
                         CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.MED, ScreenShake.ShakeDur.MED, false);
                         // Shake the screen
                         CardCrawlGame.sound.play("BLUNT_FAST");  // Play a hit sound
-                        AbstractDungeon.player.damage(new DamageInfo(null, healthdamage));
-                        AbstractRelic relic = AbstractDungeon.returnRandomScreenlessRelic(AbstractDungeon.returnRandomRelicTier());
+                        AbstractDungeon.player.damage(new DamageInfo(null, 9999));
+                        AbstractRelic relic = new PerfectCherryBlossom();
                         AbstractDungeon.getCurrRoom().spawnRelicAndObtain(this.drawX, this.drawY, relic);
-
+                        break;
+                    case 1: // Refuse
+                        this.imageEventText.updateBodyText(DESCRIPTIONS[7]);
+                        screenNum = 7;
+                        this.imageEventText.updateDialogOption(0, OPTIONS[0]);
+                        this.imageEventText.clearRemainingOptions();
                         break;
                 }
                 break;
-            case 3:
+            case 5:
+                this.imageEventText.updateBodyText(DESCRIPTIONS[6]);
+                screenNum = 6;
+                this.imageEventText.updateDialogOption(0, OPTIONS[4]);
+                this.imageEventText.clearRemainingOptions();
+                break;
+            case 6:
                 switch (buttonPressed) {
-                    case 0: // If you press the first (and this should be the only) button,
-                        openMap(); // You'll open the map and end the event.
+                    case 0:
+                        openMap(); //Open map and end event
                         break;
                 }
                 break;
-            case 4:
-                this.imageEventText.updateBodyText(DESCRIPTIONS[5]);
-                screenNum = 3;
+            case 7:
+                this.imageEventText.updateBodyText(DESCRIPTIONS[8]);
+                screenNum = 6;
                 this.imageEventText.updateDialogOption(0, OPTIONS[4]);
                 this.imageEventText.clearRemainingOptions();
                 break;
         }
     }
-
-    @Override
-    public void update() {
-        super.update();
-        if (this.pickCard && !AbstractDungeon.isScreenUp && !AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()) {
-            AbstractCard c = AbstractDungeon.gridSelectScreen.selectedCards.get(0).makeCopy();
-            c.upgrade();
-            AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(c, (float)Settings.WIDTH / 2.0F, (float)Settings.HEIGHT / 2.0F));
-            AbstractDungeon.gridSelectScreen.selectedCards.clear();
-        }
-
-    }
-
 }
