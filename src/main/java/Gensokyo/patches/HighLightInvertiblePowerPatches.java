11e835b72c576f7c75ac38298ccf3df8c912e57e
Darkglade <30379754+Darkglade@users.noreply.github.com>
2019-10-27 12:15:55 -0400

    Added Gk's highlight powers patch for the Yukari fight

---
+++ src/main/java/Gensokyo/patches/HighLightInvertiblePowerPatches.java
@@ -0,0 +1,75 @@
+package Gensokyo.patches;
+
+import Gensokyo.monsters.Yukari;
+import Gensokyo.powers.FortitudePower;
+import Gensokyo.powers.SturdyPower;
+import Gensokyo.powers.VigorPower;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.modthespire.lib.LineFinder;
+import com.evacipated.cardcrawl.modthespire.lib.Matcher;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertLocator;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.powers.FocusPower;
+import com.megacrit.cardcrawl.powers.FrailPower;
+import com.megacrit.cardcrawl.powers.GainStrengthPower;
+import com.megacrit.cardcrawl.powers.LoseStrengthPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import javassist.CtBehavior;
+
+public class HighLightInvertiblePowerPatches {
+
+    @SpirePatch(clz = AbstractPower.class, method = "renderIcons")
+    public static class HighLight {
+        @SpireInsertPatch(locator = LocatorMulti.class)
+        public static void patch(AbstractPower __instance, SpriteBatch sb, float x, float y, Color c) {
+            if (fightingYukari() && isInvertible(__instance)) {
+                Color tmp = c.cpy();
+                tmp.a = 0.5f;
+                tmp.mul(1.5f, 1.5f, 1.5f, 1.0f);
+                if (__instance.img != null) {
+                    sb.setColor(tmp);
+                    sb.draw(__instance.img, x - 12.0F, y - 12.0F, 16.0F, 16.0F, 32.0F, 32.0F, Settings.scale * 2.5F, Settings.scale * 2.5F, 0.0F, 0, 0, 32, 32, false, false);
+                } else {
+                    sb.setColor(tmp);
+                    sb.draw(__instance.region48, x - __instance.region48.packedWidth / 2.0F, y - __instance.region48.packedHeight / 2.0F, __instance.region48.packedWidth / 2.0F, __instance.region48.packedHeight / 2.0F, __instance.region48.packedWidth, __instance.region48.packedHeight, Settings.scale*1.5f, Settings.scale*1.5f, 0.0F);
+                }
+            }
+        }
+
+        private static boolean isInvertible(AbstractPower power) {
+            if (power instanceof StrengthPower || power instanceof DexterityPower || power instanceof FocusPower
+                || power instanceof VulnerablePower || power instanceof WeakPower || power instanceof FrailPower
+                || power instanceof FortitudePower || power instanceof VigorPower || power instanceof SturdyPower
+                || power instanceof GainStrengthPower || power instanceof LoseStrengthPower) {
+                return true;
+            }
+            return false;
+        }
+
+        private static boolean fightingYukari() {
+            for (AbstractMonster monster : AbstractDungeon.getCurrRoom().monsters.monsters) {
+                if (monster instanceof Yukari) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static class LocatorMulti extends SpireInsertLocator {
+            @Override
+            public int[] Locate(CtBehavior ctBehavior) throws Exception {
+                Matcher finalMatcher = new Matcher.MethodCallMatcher(SpriteBatch.class, "draw");
+                return LineFinder.findAllInOrder(ctBehavior, finalMatcher);
+            }
+        }
+    }
+}
