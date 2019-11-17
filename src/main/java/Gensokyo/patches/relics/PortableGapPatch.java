9eaca88dd9e7747b241d938f817f95de3e6584ea
Darkglade <30379754+Darkglade@users.noreply.github.com>
2019-11-16 23:40:12 -0500

    Buffed Portable Gap because reasons

--- src/main/java/Gensokyo/patches/relics/PortableGapPatch.java
+++ src/main/java/Gensokyo/patches/relics/PortableGapPatch.java
@@ -2,6 +2,8 @@ package Gensokyo.patches.relics;
 
 import Gensokyo.relics.PortableGap;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.map.MapEdge;
 import com.megacrit.cardcrawl.map.MapRoomNode;
@@ -10,6 +12,15 @@ import java.util.Iterator;
 
 public class PortableGapPatch
 {
+    private static MapRoomNode getNode(int x, int y)
+    {
+        try {
+            return CardCrawlGame.dungeon.getMap().get(y).get(x);
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
+    }
+
     @SpirePatch(
             clz= MapRoomNode.class,
             method="isConnectedTo"
@@ -28,13 +39,59 @@ public class PortableGapPatch
                     }
 
                     edge = (MapEdge)var2.next();
-                    if (AbstractDungeon.player.hasRelic(PortableGap.ID) && node.y == edge.dstY) {
+                    if (AbstractDungeon.player.hasRelic(PortableGap.ID) && node.y >= edge.dstY) {
                         return true;
                     }
-                } while(node.y != edge.dstY || !AbstractDungeon.player.hasRelic(PortableGap.ID));
+                } while(node.y < edge.dstY || !AbstractDungeon.player.hasRelic(PortableGap.ID));
             }
 
             return __result;
+        }
+
+        static int getNodeDistance(MapRoomNode start, MapRoomNode end)
+        {
+            return getNodeDistance(start, end, 1);
+        }
+
+        private static int getNodeDistance(MapRoomNode start, MapRoomNode end, int depth)
+        {
+            if (start == null) {
+                return -1;
+            }
+
+            for (MapEdge edge : start.getEdges()) {
+                MapRoomNode nextNode = getNode(edge.dstX, edge.dstY);
+                if (nextNode != null && nextNode.equals(end)) {
+                    return depth;
+                }
+                int dist = getNodeDistance(nextNode, end, depth+1);
+                if (dist != -1) {
+                    return dist;
+                }
+            }
+            return -1;
+        }
+    }
+
+    @SpirePatch(
+            cls="com.megacrit.cardcrawl.map.MapRoomNode",
+            method="playNodeSelectedSound"
+    )
+    public static class NodeSelected
+    {
+        public static void Postfix(MapRoomNode __instance)
+        {
+            if (Settings.isDebug) {
+                return;
+            }
+
+            int distance = IsConnectedTo.getNodeDistance(AbstractDungeon.getCurrMapNode(), __instance);
+            if (distance > 1) {
+                PortableGap portableGap = (PortableGap)AbstractDungeon.player.getRelic(PortableGap.ID);
+                if (portableGap != null) {
+                    portableGap.onTrigger(distance);
+                }
+            }
         }
     }
 }
--- src/main/java/Gensokyo/relics/PortableGap.java
+++ src/main/java/Gensokyo/relics/PortableGap.java
@@ -4,6 +4,8 @@ import Gensokyo.GensokyoMod;
 import Gensokyo.util.TextureLoader;
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import static Gensokyo.GensokyoMod.makeRelicOutlinePath;
 import static Gensokyo.GensokyoMod.makeRelicPath;
@@ -14,9 +16,27 @@ public class PortableGap extends CustomRelic {
 
     private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("PortableGap.png"));
     private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("PortableGap.png"));
+    private boolean justUsed = false;
+    private int distanceTraveled = 0;
 
     public PortableGap() {
         super(ID, IMG, OUTLINE, RelicTier.SPECIAL, LandingSound.MAGICAL);
+    }
+
+    @Override
+    public void onEnterRoom(AbstractRoom room)
+    {
+        if (justUsed) {
+            justUsed = false;
+            AbstractDungeon.floorNum += distanceTraveled - 1;
+            distanceTraveled = 0;
+        }
+    }
+
+    public void onTrigger(int distance)
+    {
+        justUsed = true;
+        distanceTraveled = distance;
     }
 
     // Description
