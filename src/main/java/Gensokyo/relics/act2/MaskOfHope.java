bcf2a02b736154194ca2309961b16aa023a6c696
Darkglade <30379754+Darkglade@users.noreply.github.com>
2020-03-11 22:51:09 -0400

    reworked Mask of Hope relic

--- src/main/java/Gensokyo/relics/act2/MaskOfHope.java
+++ src/main/java/Gensokyo/relics/act2/MaskOfHope.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import static Gensokyo.GensokyoMod.makeRelicOutlinePath;
 import static Gensokyo.GensokyoMod.makeRelicPath;
@@ -18,17 +19,40 @@ public class MaskOfHope extends CustomRelic {
     private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("MaskOfHope.png"));
     private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("MaskOfHope.png"));
 
-    private static final int TEMP_HP = 8;
+    private static final int TEMP_HP = 10;
+    private boolean usedThisCombat;
 
     public MaskOfHope() {
         super(ID, IMG, OUTLINE, RelicTier.SPECIAL, LandingSound.MAGICAL);
     }
 
     @Override
-    public void atBattleStartPreDraw() {
-        this.flash();
-        this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
-        AbstractDungeon.actionManager.addToBottom(new AddTemporaryHPAction(AbstractDungeon.player, AbstractDungeon.player, TEMP_HP));
+    public void atPreBattle() {
+        usedThisCombat = false;
+        this.pulse = true;
+        this.beginPulse();
+    }
+
+    @Override
+    public void wasHPLost(int damageAmount) {
+        if (damageAmount > 0 && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT && !usedThisCombat) {
+            this.flash();
+            this.pulse = false;
+            this.addToTop(new AddTemporaryHPAction(AbstractDungeon.player, AbstractDungeon.player, TEMP_HP));
+            this.addToTop(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+            usedThisCombat = true;
+            this.grayscale = true;
+        }
+    }
+
+    @Override
+    public void justEnteredRoom(AbstractRoom room) {
+        this.grayscale = false;
+    }
+
+    @Override
+    public void onVictory() {
+        this.pulse = false;
     }
 
     @Override
