be9d186b2350f2e5a75b65644b4c89fd3e062be0
Darkglade <30379754+Darkglade@users.noreply.github.com>
2020-11-21 15:22:46 -0500

    remilia is now properly untargetable by the player

---
+++ src/main/java/Gensokyo/patches/MakeRemiliaUntargetable.java
@@ -0,0 +1,35 @@
+package Gensokyo.patches;
+
+import Gensokyo.monsters.act3.Remilia;
+import com.evacipated.cardcrawl.modthespire.lib.ByRef;
+import com.evacipated.cardcrawl.modthespire.lib.LineFinder;
+import com.evacipated.cardcrawl.modthespire.lib.Matcher;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertLocator;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.monsters.MonsterGroup;
+import javassist.CtBehavior;
+
+@SpirePatch(
+        clz = AbstractPlayer.class,
+        method = "updateSingleTargetInput"
+
+)
+// A patch to make Remilia untargetable by the player
+public class MakeRemiliaUntargetable {
+    @SpireInsertPatch(locator = MakeRemiliaUntargetable.Locator.class, localvars = {"hoveredMonster"})
+    public static void MakeHoveredMonsterNull(AbstractPlayer instance, @ByRef AbstractMonster[] hoveredMonster) {
+        if (hoveredMonster[0] != null && hoveredMonster[0].id.equals(Remilia.ID)) {
+            hoveredMonster[0] = null;
+        }
+    }
+    private static class Locator extends SpireInsertLocator {
+        @Override
+        public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+            Matcher finalMatcher = new Matcher.MethodCallMatcher(MonsterGroup.class, "areMonstersBasicallyDead");
+            return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+        }
+    }
+}
