a388748b5ed25efcc71bdf8e8f7d303771ab8104
Darkglade <30379754+Darkglade@users.noreply.github.com>
2020-12-06 12:06:35 -0500

    set up yumeko

--- src/main/java/Gensokyo/monsters/act3/Shinki/Shinki.java
+++ src/main/java/Gensokyo/monsters/act3/Shinki/Shinki.java
@@ -26,6 +26,7 @@ import com.megacrit.cardcrawl.monsters.EnemyMoveInfo;
 import com.megacrit.cardcrawl.screens.DeathScreen;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -83,6 +84,8 @@ public class Shinki extends AbstractSpriterMonster
     public void usePreBattleAction() {
         //AbstractDungeon.getCurrRoom().playBgmInstantly("Wind God Girl");
         delusionList.add(new Alice(-480.0f, 0.0f, this));
+        delusionList.add(new Yumeko(-480.0f, 0.0f, this));
+        Collections.shuffle(delusionList, AbstractDungeon.monsterRng.random);
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(this, this, new UnstableReality(this)));
     }
 
---
+++ src/main/java/Gensokyo/monsters/act3/Shinki/Yumeko.java
@@ -0,0 +1,170 @@
+package Gensokyo.monsters.act3.Shinki;
+
+import Gensokyo.BetterSpriterAnimation;
+import Gensokyo.CustomIntents.IntentEnums;
+import Gensokyo.GensokyoMod;
+import Gensokyo.actions.UsePreBattleActionAction;
+import Gensokyo.powers.act1.VigorPower;
+import Gensokyo.powers.act3.BurdenOfFailure;
+import Gensokyo.powers.act3.DollJudgement;
+import com.badlogic.gdx.graphics.Color;
+import com.brashmonkey.spriter.Animation;
+import com.brashmonkey.spriter.Player;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.TalkAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.actions.common.RollMoveAction;
+import com.megacrit.cardcrawl.actions.common.SpawnMonsterAction;
+import com.megacrit.cardcrawl.actions.common.SuicideAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.status.Burn;
+import com.megacrit.cardcrawl.cards.status.Dazed;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.MonsterStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.monsters.EnemyMoveInfo;
+import com.megacrit.cardcrawl.powers.FrailPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.vfx.BorderFlashEffect;
+import com.megacrit.cardcrawl.vfx.combat.LightningEffect;
+import com.megacrit.cardcrawl.vfx.combat.SmallLaserEffect;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Yumeko extends AbstractShinkiDelusion
+{
+    public static final String ID = GensokyoMod.makeID("Yumeko");
+    private static final MonsterStrings monsterStrings = CardCrawlGame.languagePack.getMonsterStrings(ID);
+    public static final String NAME = monsterStrings.NAME;
+    public static final String[] MOVES = monsterStrings.MOVES;
+    public static final String[] DIALOG = monsterStrings.DIALOG;
+
+    private boolean firstMove = true;
+    private static final byte ATTACK = 0;
+    private static final byte DEBUFF_ATTACK = 1;
+    private static final byte DEFEND_DEBUFF = 2;
+
+    private static final int ATTACK_DMG = 20;
+    private static final int A4_ATTACK_DMG = 22;
+    private int attackDmg;
+
+    private static final int DEBUFF_ATTACK_DMG = 14;
+    private static final int A4_DEBUFF_ATTACK_DMG = 15;
+    private int debuffAttackDmg;
+
+    private static final int STATUS_AMT = 2;
+
+    private static final int A9_BLOCK = 12;
+    private static final int BLOCK = 10;
+    private int block;
+
+    private static final int DEBUFF_AMT = 1;
+
+    private static final int POWER_AMT = 30;
+
+    private static final int HP = 300;
+    private static final int A9_HP = 330;
+
+    public boolean powerTriggered = false;
+    private Map<Byte, EnemyMoveInfo> moves;
+
+    public Yumeko(final float x, final float y, Shinki shinki) {
+        super(NAME, ID, HP, -5.0F, 0, 230.0f, 225.0f, null, x, y);
+        this.animation = new BetterSpriterAnimation("GensokyoResources/images/monsters/Yumeko/Spriter/YumekoAnimation.scml");
+        this.type = EnemyType.BOSS;
+        this.dialogX = (this.hb_x - 70.0F) * Settings.scale;
+        this.dialogY -= (this.hb_y - 55.0F) * Settings.scale;
+        this.shinki = shinki;
+        this.event1 = new AliceEvent1(shinki);
+        this.event2 = new AliceEvent2(shinki);
+        this.event3 = new AliceEvent3(shinki);
+        if (AbstractDungeon.ascensionLevel >= 9) {
+            setHp(A9_HP);
+            this.block = A9_BLOCK;
+        } else {
+            setHp(HP);
+            this.block = BLOCK;
+        }
+
+        if (AbstractDungeon.ascensionLevel >= 4) {
+            attackDmg = A4_ATTACK_DMG;
+            debuffAttackDmg = A4_DEBUFF_ATTACK_DMG;
+        } else {
+            attackDmg = ATTACK_DMG;
+            debuffAttackDmg = DEBUFF_ATTACK_DMG;
+        }
+
+        this.moves = new HashMap<>();
+        this.moves.put(ATTACK, new EnemyMoveInfo(ATTACK, Intent.ATTACK, attackDmg, 0, false));
+        this.moves.put(DEBUFF_ATTACK, new EnemyMoveInfo(DEBUFF_ATTACK, Intent.ATTACK_DEBUFF, debuffAttackDmg, 0, false));
+        this.moves.put(DEFEND_DEBUFF, new EnemyMoveInfo(DEFEND_DEBUFF, Intent.DEFEND_DEBUFF, -1, 0, false));
+    }
+
+    @Override
+    public void usePreBattleAction() {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(this, this, new BurdenOfFailure(this, POWER_AMT, this)));
+    }
+
+    @Override
+    public void takeTurn() {
+        if (this.firstMove) {
+            AbstractDungeon.actionManager.addToBottom(new TalkAction(this, DIALOG[0]));
+            firstMove = false;
+        }
+        DamageInfo info = new DamageInfo(this, moves.get(this.nextMove).baseDamage, DamageInfo.DamageType.NORMAL);
+        if(info.base > -1) {
+            info.applyPowers(this, AbstractDungeon.player);
+        }
+        switch (this.nextMove) {
+            case ATTACK: {
+                useFastAttackAnimation();
+                AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, info, AbstractGameAction.AttackEffect.SLASH_HEAVY));
+                break;
+            }
+            case DEBUFF_ATTACK: {
+                useFastAttackAnimation();
+                AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, info, AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+                AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(new Dazed(), STATUS_AMT, true, true));
+                break;
+            }
+            case DEFEND_DEBUFF: {
+                addToBot(new GainBlockAction(this, block));
+                addToBot(new ApplyPowerAction(AbstractDungeon.player, this, new VulnerablePower(AbstractDungeon.player, DEBUFF_AMT, true), DEBUFF_AMT));
+                addToBot(new ApplyPowerAction(AbstractDungeon.player, this, new FrailPower(AbstractDungeon.player, DEBUFF_AMT, true), DEBUFF_AMT));
+                break;
+            }
+        }
+        AbstractDungeon.actionManager.addToBottom(new RollMoveAction(this));
+    }
+
+    public void setMoveShortcut(byte next) {
+        EnemyMoveInfo info = this.moves.get(next);
+        this.setMove(MOVES[next], next, info.intent, info.baseDamage, info.multiplier, info.isMultiDamage);
+    }
+
+    @Override
+    protected void getMove(final int num) {
+        if (powerTriggered) {
+            setMoveShortcut(DEFEND_DEBUFF);
+        } else if (lastMove(ATTACK)){
+            setMoveShortcut(DEBUFF_ATTACK);
+        } else {
+            setMoveShortcut(ATTACK);
+        }
+    }
+
+    @Override
+    public void die(boolean triggerRelics) {
+        ((BetterSpriterAnimation)this.animation).startDying();
+        super.die(triggerRelics);
+    }
+}
