56c852e2a922275940bb553e6d54a8159b6f82b2
Darkglade <30379754+Darkglade@users.noreply.github.com>
2020-12-09 22:11:38 -0500

    refactor old code to make use of CARD MODS

--- src/main/java/Gensokyo/powers/act3/UnknownCurse.java
+++ src/main/java/Gensokyo/powers/act3/UnknownCurse.java
@@ -10,6 +10,9 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class UnknownCurse extends AbstractPower {
     public static final String POWER_ID = GensokyoMod.makeID("UnknownCurse");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
@@ -31,23 +34,31 @@ public class UnknownCurse extends AbstractPower {
     @Override
     public void onInitialApplication() {
         if (owner == AbstractDungeon.player) {
+            ArrayList<AbstractCard> allCards = new ArrayList<>();
+            allCards.addAll(AbstractDungeon.player.drawPile.group);
+            allCards.addAll(AbstractDungeon.player.discardPile.group);
+            allCards.addAll(AbstractDungeon.player.hand.group);
+            allCards.addAll(AbstractDungeon.player.exhaustPile.group);
+            Collections.shuffle(allCards, AbstractDungeon.cardRandomRng.random);
+            ArrayList<AbstractCard> obscuredCards = new ArrayList<>(allCards.subList(0, (int)(allCards.size() * 0.5f)));
+
             for (AbstractCard card : AbstractDungeon.player.drawPile.group) {
-                if (AbstractDungeon.cardRandomRng.randomBoolean() || alwaysObscure) {
+                if (alwaysObscure || obscuredCards.contains(card)) {
                     CardModifierManager.addModifier(card, new ObscureMod());
                 }
             }
             for (AbstractCard card : AbstractDungeon.player.discardPile.group) {
-                if (AbstractDungeon.cardRandomRng.randomBoolean() || alwaysObscure) {
+                if (alwaysObscure || obscuredCards.contains(card)) {
                     CardModifierManager.addModifier(card, new ObscureMod());
                 }
             }
             for (AbstractCard card : AbstractDungeon.player.hand.group) {
-                if (AbstractDungeon.cardRandomRng.randomBoolean() || alwaysObscure) {
+                if (alwaysObscure || obscuredCards.contains(card)) {
                     CardModifierManager.addModifier(card, new ObscureMod());
                 }
             }
             for (AbstractCard card : AbstractDungeon.player.exhaustPile.group) {
-                if (AbstractDungeon.cardRandomRng.randomBoolean() || alwaysObscure) {
+                if (alwaysObscure || obscuredCards.contains(card)) {
                     CardModifierManager.addModifier(card, new ObscureMod());
                 }
             }
